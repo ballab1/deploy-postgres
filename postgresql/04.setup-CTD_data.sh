2d8dcb3d31f9626167536cc0c67dd10e8a763645
Ballantyne, Bob <ballantyne.robert@gmail.com>
2021-06-06 08:13:37 -0400

    first commit

--- postgresql/04.setup-CTD_data.sh
+++ postgresql/04.setup-CTD_data.sh
@@ -4,10 +4,22 @@ declare tools="$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )"
 declare pg_data="${PG_DATA:-/var/lib/postgresql/data}"
 declare -r conf_file="${pg_data}/postgresql.conf "
 declare -r conf_entry="shared_preload_libraries = 'timescaledb'"
+declare do_restart
 
 if ! grep -q "$conf_entry" "$conf_file" ; then
-    echo "Adding shared_preload_libraries = 'timescaledb' to ${pg_data}/postgresql.conf"
-    echo "$conf_entry" >> "$conf_file"
+    echo "Adding ${conf_entry}' to ${pg_data}/postgresql.conf"
+    s -i "\$ a $conf_entry" "$conf_file"
+    do_restart='restart'
+fi
+
+if -f "$conf_file" ; then
+    echo "changing log directory"
+    sed -E -i -e '^.*log_directory\s*=.*$|log_directory = /var/log|' "$conf_file"
+    do_restart='restart'
+fi
+
+
+if [ "${do_restart:-}" ]; then
     echo "Restarting postgresql"
     pg_ctl restart
 fi
